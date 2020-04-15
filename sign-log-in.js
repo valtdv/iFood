752f69fe43c7a52a9369ea3cdba21dbb2ac8e85d
valeria <valeriaososal@gmail.com>
2020-04-15 00:16:41 -0500

    pagina de inicio y formularios

---
+++ sign-log-in.js
@@ -0,0 +1,9 @@
+function signin(){
+    document.title = "iFood - Registrarse";
+    document.getElementById("form").innerHTML = "New text!";
+}
+
+function login(){
+    document.title = "iFood - Iniciar sesión";
+    document.getElementById("form").innerHTML = "New text!";
+}
