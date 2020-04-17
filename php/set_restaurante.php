4e48628108ab4488bc9a5197ce0875fda8253b50
ettop <ettop@localhost.localdomain>
2020-04-17 11:19:49 -0500

    set usuer and restaurants

--- php/set_restaurante.php
+++ php/set_restaurante.php
@@ -1,12 +1,11 @@
 <?php
     include('configuration.php');
 
-    $sql= "INSERT INTO Restaurante VALUES (11111,'El Sazon casero','cra 51b #35-05', 3034598, 'sazoncasero@hotmail.com');
-    ";
+    $sql= "INSERT INTO Restaurante VALUES (11111,'El Sazon casero','cra 51b #35-05', 3034598, 'sazoncasero@hotmail.com')";
 
     if (mysqli_query($connection, $sql)) {
         echo "New record created successfully";
     } else {
-        echo "Error: " . $dml . "<br>" . mysqli_error($conn);
+        echo "Error: " . $sql . "<br>" . mysqli_error($connection);
     }    
 ?>
--- php/set_usuario.php
+++ php/set_usuario.php
@@ -1,12 +1,11 @@
 <?php
     include('configuration.php');
 
-    $sql= "INSERT INTO Usuario VALUES (1234567,'contra','Diego','Albor','Rojas','cra 34 #87-09','diegoaalborrojas@gmail.com');
-    ";
+    $sql= "INSERT INTO Usuario VALUES (1234567,'contra','Diego','Albor','Rojas','cra 34 #87-09','diegoaalborrojas@gmail.com')";
 
     if (mysqli_query($connection, $sql)) {
         echo "New record created successfully";
     } else {
-        echo "Error: " . $dml . "<br>" . mysqli_error($conn);
+        echo "Error: " . $sql . "<br>" . mysqli_error($connection);
     }    
 ?>
