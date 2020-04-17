ba3db6063920c853f1a79f8ba7fa9b7b8cfb777c
ettop <ettop@localhost.localdomain>
2020-04-17 11:13:24 -0500

    set usuer and restaurants

--- php/set_restaurante.php
+++ php/set_restaurante.php
@@ -4,7 +4,7 @@
     $sql= "INSERT INTO Restaurante VALUES (11111,'El Sazon casero','cra 51b #35-05', 3034598, 'sazoncasero@hotmail.com');
     ";
 
-    if (mysqli_query($conn, $sql)) {
+    if (mysqli_query($connection, $sql)) {
         echo "New record created successfully";
     } else {
         echo "Error: " . $dml . "<br>" . mysqli_error($conn);
--- php/set_usuario.php
+++ php/set_usuario.php
@@ -4,7 +4,7 @@
     $sql= "INSERT INTO Usuario VALUES (1234567,'contra','Diego','Albor','Rojas','cra 34 #87-09','diegoaalborrojas@gmail.com');
     ";
 
-    if (mysqli_query($conn, $sql)) {
+    if (mysqli_query($connection, $sql)) {
         echo "New record created successfully";
     } else {
         echo "Error: " . $dml . "<br>" . mysqli_error($conn);
