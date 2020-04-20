30d21e55633ce574056dfc8d6f666cf839ab8611
Diego Antonio Albor Rojas <daalbor@uninorte.edu.co>
2020-04-20 12:00:45 -0500

    Added changed

--- php/set_usuario.php
+++ php/set_usuario.php
@@ -7,10 +7,11 @@
     $direccion = $_POST['direccion'];
     $email1 = $_POST['email1'];
     $password1 = $_POST['password1'];
-    $membresia = false;
+    $membresia = '0';
     $telefono1 = $_POST['telefono1'];
+    
 
-    $sql = "INSERT INTO Cliente VALUES ('$password1','$membresia','$nombre','$apellido1','$apellido2','$email1','$telefono1','$direccion')";
+    $sql = "INSERT INTO Cliente VALUES (NULL,'$password1','$membresia','$nombre','$apellido1','$apellido2','$email1','$telefono1','$direccion')";
     if (mysqli_query($connection, $sql)) {
         echo "Cuenta creada exitosamente";
     } else {
