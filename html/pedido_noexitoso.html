00316012d9e76e04bbb20dbe0147546862ca85d8
lpotte <lpotte@uninorte.edu.co>
2020-05-25 15:05:36 -0500

    buenas prácticas en php's de conexión

---
+++ html/pedido_noexitoso.html
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MyFood - Un error ha ocurrdo</title>
+    <link rel='stylesheet' href='styles.css' />
+    <link
+            rel='stylesheet'
+            href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'
+            integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh'
+            crossorigin='anonymous'
+    />
+</head>
+<body class='pattern'>
+<section id='form' class='d-flex justify-content-center'>
+    <article class='bg-light' id='formulario'>
+        <h1>Un error ha ocurrdo</h1>
+        <p>Ha ocurrido un error en tu registro, notifica a los administradores</p>
+        <button class='btn btn-danger' type='submit' onclick=location='../index.php'> Aceptar </button>
+    </article>
+</section>
+<script
+        src='https://code.jquery.com/jquery-3.4.1.slim.min.js'
+        integrity='sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n'
+        crossorigin='anonymous'
+></script>
+<script
+        src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'
+        integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo'
+        crossorigin='anonymous'
+></script>
+<script
+        src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js'
+        integrity='sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6'
+        crossorigin='anonymous'
+></script>
+</body>
+</html>
--- php/set_usuario.php
+++ php/set_usuario.php
@@ -13,48 +13,11 @@
 
     $sql = "INSERT INTO Cliente VALUES (NULL, '$password1','$membresia','$nombre','$apellido1','$apellido2','$email1','$telefono1','$direccion')";
     if (mysqli_query($connection, $sql)) {
-        echo "Cuenta creada exitosamente";
+        //echo "Cuenta creada exitosamente";
+        include('../html/reg_exitoso.html');
     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+        //echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+        include('../html/error_registro.html');
     }
     mysqli_close($connection);
-    echo "<!DOCTYPE html>
-    <html>
-      <head>
-        <title>MyFood - Registro exitoso</title>
-        <link rel='stylesheet' href='styles.css' />
-        <link
-          rel='stylesheet'
-          href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'
-          integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh'
-          crossorigin='anonymous'
-        />
-      </head>
-      <body class='pattern'>
-        <section id='form' class='d-flex justify-content-center'>
-        <article class='bg-light' id='formulario'>
-           <h1>¡Registro exitoso!</h1>
-           <p>Tú registro ha sido exitoso, ya puedes ingresar sesión en MyFood</p>
-           <div class='d-flex justify-content-center'>
-             <button class='btn btn-danger' type='submit' onclick=location='../index.php'> Aceptar </button>
-           </div>
-        </article>
-        </section>
-        <script
-          src='https://code.jquery.com/jquery-3.4.1.slim.min.js'
-          integrity='sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n'
-          crossorigin='anonymous'
-        ></script>
-        <script
-          src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'
-          integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo'
-          crossorigin='anonymous'
-        ></script>
-        <script
-          src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js'
-          integrity='sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6'
-          crossorigin='anonymous'
-        ></script>
-      </body>
-    </html>";
 ?>
