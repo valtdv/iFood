d97c05bf0ae608bf5fdd6d0d8d0a66eedcde12fb
Diego Albor <diegoaalborrojas@gmail.com>
2020-05-26 23:05:42 -0500

    JQuery Added

--- adminview.php
+++ adminview.php
@@ -6,6 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="./css/adminview_styles.css" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     </head>
     <body>
         <header>
@@ -21,12 +23,12 @@
             <div class="bg-light border-right" id="sidebar-wrapper">
                 <div class="sidebar-heading">Menú</div>
                 <div class="list-group list-group-flush">
-                    <a href="#" class="list-group-item list-group-item-action bg-light">Ordenes Activas</a>
-                    <a href="#" class="list-group-item list-group-item-action bg-light">Pagos Realizados</a>
-                    <a href="#" class="list-group-item list-group-item-action bg-light">Gestión de Clientes</a>
-                    <a href="#" class="list-group-item list-group-item-action bg-light">Bodega de Ingredientes</a>
-                    <a href="#" class="list-group-item list-group-item-action bg-light">Gestión de Platos</a>
-                    <a href="#" class="list-group-item list-group-item-action bg-light">Ordenes Rechazadas</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-light" id="Ordenes">Ordenes Activas</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-light" id="Pagos">Pagos Realizados</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-light" id="Gestion">Gestión de Clientes</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-light" id="Ingredientes">Bodega de Ingredientes</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-light" id="Platos">Gestión de Platos</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-light" id="Rechazos">Ordenes Rechazadas</a>
                 </div>
             </div>
             <div id="page-content-wrapper">
@@ -38,12 +40,32 @@
 
         </div>
       
-        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        
         <script>
             $("#menu-toggle").click(function(e) {
                 e.preventDefault();
                 $("#wrapper").toggleClass("toggled");
+            });
+
+            $(document).ready(function(){
+                $('#Ordenes').click(function(){
+                    $("#page-content-wrapper").load("orden_admin.php");
+                });
+                $('#Pagos').click(function(){
+                    $("#page-content-wrapper").load("pagos_admin.php");
+                });
+                $('#Gestion').click(function(){
+                    $("#page-content-wrapper").load("gestion_admin.php");
+                });
+                $('#Ingredientes').click(function(){
+                    $("#page-content-wrapper").load("ingredientes_admin.php");
+                });
+                $('#Platos').click(function(){
+                    $("#page-content-wrapper").load("platos_admin.php");
+                });
+                $('#Rechazos').click(function(){
+                    $("#page-content-wrapper").load("rechazos_admin.php");
+                });
             });
         </script>
     </body>
---
+++ ingredientes_admin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MyFood - Inicio</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="./css/adminview_styles.css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+    </head>
+    <body>
+        <h1 class="seccion_titles">Bodega de Ingredientes</h1>
+        <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+    </body>
+</html>
